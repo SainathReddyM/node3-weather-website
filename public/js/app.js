7e3a06d59d2395822c6091da42f4d93820020332
SainathReddyM <sainath@DESKTOP-LC5GIF7.localdomain>
2020-05-02 13:38:01 +0530

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("client side javascript code");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const loading = document.querySelector("#loading");
